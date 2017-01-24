f8f734d662c15b13acb2285140cbab21dba88809
Seokmin Lee <essemfly@gmail.com>
2017-01-24 11:55:29 +0900

    Make flow player work for http live streaming

---
+++ src/home/FlowPlayer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import flowplayer from 'flowplayer';
+
+export default class FlowPlayer extends React.Component {
+
+    componentDidMount() { this.checkIfVideoNeedsInstallation(); }
+    componentDidUpdate() { this.checkIfVideoNeedsInstallation(); }
+
+    checkIfVideoNeedsInstallation = () => {
+        if(!this.props.src)
+            return;
+
+        this.loadVideo();
+    }
+
+    loadVideo = () => {
+        if(this.player || !this.props.src)
+            return;
+
+        let node = ReactDOM.findDOMNode(this.refs.flowplayer);
+        if(!node)
+            return;
+
+        this.player = flowplayer(node, {
+            autoplay: true,
+            live: true,
+            splash: false,
+            clip: {
+                sources: [
+                      { type: "application/x-mpegurl",
+                        src:  this.props.src }
+                ]
+            }
+        });
+
+        this.player.bind("error", function (e, api, err) {
+          console.log('error', e, api, err);
+        });
+    }
+
+    render() {
+        return <div ref="flowplayer" />;
+    }
+}
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import FlowPlayer from './FlowPlayer';
 
 const navStyle = {
-  width: '100%',
+  width: '60%',
+  marginRight: 'auto',
+  marginLeft: 'auto',
   height: 'inherit',
 }
 
@@ -11,6 +14,7 @@ class Home extends Component {
     return (
       <div style={navStyle}>
         나는 홈화면이라 해
+        <FlowPlayer src="http://livem.gsshop.com/gsmyshop_sd/_definst_/gsmyshop_sd.stream/playlist.m3u8"/>
       </div>
     );
   }
