87cd240f00bbc60d97843db14dd82b82fac69552
Seokmin Lee <essemfly@gmail.com>
2017-01-24 19:18:00 +0900

    Make container layout

--- src/home/HomeComponent.js
+++ src/home/HomeComponent.js
@@ -2,19 +2,43 @@ import React, { Component } from 'react';
 import FlowPlayer from './FlowPlayer';
 
 const navStyle = {
-  width: '60%',
+  width: '100%',
+  maxWidth: '980px',
+  minWidth: '780px',
+  display: 'table',
   marginRight: 'auto',
   marginLeft: 'auto',
-  height: 'inherit',
 }
-
 
 class HomeComponent extends Component {
   render() {
     return (
       <div style={navStyle}>
-        나는 홈화면이라 해
-        <FlowPlayer src="http://livem.gsshop.com/gsmyshop_sd/_definst_/gsmyshop_sd.stream/playlist.m3u8"/>
+        <div style={{ display: 'table-cell', }}>
+          <FlowPlayer src="http://livem.gsshop.com/gsmyshop_sd/_definst_/gsmyshop_sd.stream/playlist.m3u8"/>
+        </div>
+        <div style={{ width: '110px', height: '100%', display: 'table-cell', verticalAlign: 'top',}}>
+          <ul style={{ overflow: 'scroll', height: '100%',}}>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 1 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 2 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 3 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 4 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 5 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 6 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
+            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
+          </ul>
+        </div>      
       </div>
     );
   }
--- src/layout/HeaderComponent.js
+++ src/layout/HeaderComponent.js
@@ -1,14 +1,23 @@
 import React, { PropTypes } from 'react'
 import SearchComponent from './SearchComponent';
 
-const headerStyle = {
+const headerLayoutStyle = {
+  width: '100%',
+  borderBottom: '1px solid #bbb',
   position: 'absolute',
   top: 0,
-  borderBottom: '1px solid #bbb',
   height: '80px',
   backgroundColor: '#FFFFFF',
+}
+
+const headerStyle = {  
   display: 'table',
-  width: '100%', 
+  width: '100%',
+  maxWidth: '980px',
+  minWidth: '780px',
+  height: '80px',
+  marginRight: 'auto',
+  marginLeft: 'auto',
 }
 
 const logoDivStyle = {
@@ -18,6 +27,7 @@ const logoDivStyle = {
 }
 
 const logoStyle = {
+  float: 'left',
   height: '26px',
   lineHeight: '80px',
 }
@@ -30,12 +40,14 @@ const searchBoxStyle = {
 }
 
 const HeaderComponent = ({  keyword, onSearchClick, onChangeKeyword }) => (
-  <div style={headerStyle}>
-    <div style={logoDivStyle}>
-      <img src="http://cache.hsmoa.com/media/img/web/logo_hsmoa.png" style={logoStyle} alt=''/>
-    </div>
-    <div style={searchBoxStyle}>
-      <SearchComponent keyword={keyword} onChangeKeyword={onChangeKeyword} onSearchClick={onSearchClick} />
+  <div style={headerLayoutStyle}>
+    <div style={headerStyle}>
+      <div style={logoDivStyle}>
+        <img src="http://cache.hsmoa.com/media/img/web/logo_hsmoa.png" style={logoStyle} alt=''/>
+      </div>
+      <div style={searchBoxStyle}>
+        <SearchComponent keyword={keyword} onChangeKeyword={onChangeKeyword} onSearchClick={onSearchClick} />
+      </div>
     </div>
   </div>
 )
