200087f35b8db33924323702c2b418495aaaaed8
Seokmin Lee <essemfly@gmail.com>
2017-01-26 17:07:05 +0900

    Change logo images in home page

--- src/home/HomeComponent.js
+++ src/home/HomeComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import FlowPlayer from './FlowPlayer';
 
 const navStyle = {
@@ -10,38 +10,39 @@ const navStyle = {
   marginLeft: 'auto',
 }
 
-class HomeComponent extends Component {
-  render() {
-    return (
-      <div style={navStyle}>
-        <div style={{ display: 'table-cell', }}>
-          <FlowPlayer src="http://livem.gsshop.com/gsmyshop_sd/_definst_/gsmyshop_sd.stream/playlist.m3u8"/>
-        </div>
-        <div style={{ width: '110px', height: '100%', display: 'table-cell', verticalAlign: 'top',}}>
-          <ul style={{ overflow: 'scroll', height: '100%',}}>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 1 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 2 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 3 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 4 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 5 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 6 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 7 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 8 </li>
-            <li style={{ height: '39px', background: '#FFFFFF', lineHeight: '39px', boxShadow: 'inset 0 -1px 0 0 #EEEEEE', }}> 9 </li>
-          </ul>
-        </div>      
-      </div>
-    );
-  }
+const listStyle = {
+  height: '39px',
+  background: '#FFFFFF',
+  lineHeight: '39px',
+  boxShadow: 'inset 0 -1px 0 0 #EEEEEE',
+}
+
+const logoStyle = {
+  height: '25px',
+  verticalAlign: 'middle',
+}
+
+const HomeComponent = ({ channels, onAir }) => (
+  <div style={navStyle}>
+    <div style={{ display: 'table-cell', }}>
+      <FlowPlayer src="http://livem.gsshop.com/gsmyshop_sd/_definst_/gsmyshop_sd.stream/playlist.m3u8"/>
+    </div>
+    <div style={{ width: '110px', height: '100%', display: 'table-cell', verticalAlign: 'top',}}>
+      <ul style={{ overflow: 'scroll', height: '100%',}}>
+        { channels.map((channel) => 
+          <li style={listStyle} key={channel.id}>
+            <img style={logoStyle} src={'http://cache.m.ui.hsmoa.com/media/logo3/logo_big_' + channel.crawl_site + '.png'}/>
+          </li>)
+
+        }
+      </ul>
+    </div>      
+  </div>
+)
+
+HomeComponent.propTypes = {
+  onAirIndex: PropTypes.number,
+  channels: PropTypes.array,
 }
 
 export default HomeComponent;
