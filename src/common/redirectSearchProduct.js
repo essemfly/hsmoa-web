bcb7c5fe30d57703fa413d63f1bb2687ac01a3aa
Seokmin Lee <essemfly@gmail.com>
2017-02-28 10:27:53 +0900

    Fix redirect url in search page

--- src/common/redirectSearchProduct.js
+++ src/common/redirectSearchProduct.js
@@ -1,6 +1,6 @@
 const redirectSearch = (from, site, good_id, url) => {
     let encoded_url = encodeURIComponent(url)
-    return `http://hsmoa.com/redirect?type=searchclick&from=${{from}}&site=${{site}}&pid=${{good_id}}&hsuri=${{encoded_url}}`
+    return `http://hsmoa.com/redirect?type=searchclick&from=${from}&site=${site}&pid=${good_id}&hsuri=${encoded_url}`
 }
 
 export default redirectSearch
