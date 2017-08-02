7342f5ad11c934d16e5b049fc60b7db7550c1f22
essemfly <essemfly@gmail.com>
2017-08-02 16:49:38 +0900

    Make pass test with react-slick config file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Router, Route, browserHistory, IndexRoute} from 'react-router';
+import { Provider } from 'react-redux';
+import { rootStore } from './rootReducer';
+import { HomeContainer } from './home';
+import { ProductContainer } from './product';
+import { SearchContainer } from './search';
+import { Top100Container } from './top100';
+import { ScheduleContainer } from './schedule';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  const history = syncHistoryWithStore(browserHistory, rootStore)
+
+  ReactDOM.render((
+  <Provider store={rootStore}>
+    <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={HomeContainer}/>
+        <Route path="top" component={Top100Container}/>
+        <Route path="schedule" component={ScheduleContainer} />
+        <Route path="i/:entryId" component={ProductContainer} />
+        <Route path="search/:keyword" component={SearchContainer} />
+      </Route>
+    </Router>
+  </Provider>
+  ),
+  div);
 });
